4bd8599352577c3d23555a1ecd2064bfcc802cae
Ubuntu <ubuntu@ip-172-31-80-69.ec2.internal>
2023-05-15 10:18:27 +0000

    creation of the age and gender evaluation sets preparation script

--- age_gender_evaluation_sets_prep.py
+++ age_gender_evaluation_sets_prep.py
@@ -11,10 +11,10 @@ evaluation = pd.read_csv(args.data + '/sw/validated_without_dialect_domain_eval.
 evaluation[evaluation['gender'] == 'male'].to_csv(args.data + '/sw/eval_gender_male.tsv', index=False, sep='\t')
 evaluation[evaluation['gender'] == 'female'].to_csv(args.data + '/sw/eval_gender_female.tsv', index=False, sep='\t')
 evaluation[evaluation['age'] == 'twenties'].to_csv(args.data + '/sw/eval_age_20s.tsv', index=False, sep='\t')
-pd.concat([evaluation[evaluation['age'] == 'thirties'], evaluation[evaluation['age'] == 'fourties'], evaluation[evaluation['age'] == 'fifties'], evaluation[evaluation['age'] == 'sixties']]).to_csv(args.data + '/sw/eval_age_o30s.csv', index=False, sep='\t')
+pd.concat([evaluation[evaluation['age'] == 'thirties'], evaluation[evaluation['age'] == 'fourties'], evaluation[evaluation['age'] == 'fifties'], evaluation[evaluation['age'] == 'sixties']]).to_csv(args.data + '/sw/eval_age_o30s.tsv', index=False, sep='\t')
 
 # save my age x gender evaluation sets
 evaluation.loc[(evaluation['gender'] == 'male') & (evaluation['age'] == 'twenties')].to_csv(args.data + '/sw/eval_age_gender_20s_male.tsv', index=False, sep='\t')
 evaluation.loc[(evaluation['gender'] == 'female') & (evaluation['age'] == 'twenties')].to_csv(args.data + '/sw/eval_age_gender_20s_female.tsv', index=False, sep='\t')
-evaluation.loc[(evaluation['gender'] == 'female') & ~(eval0['age'] == 'twenties') & ~(eval0['age'] == 'teens')].to_csv(args.data + '/sw/eval_age_gender_o30s_female.csv', index=False, sep='\t')
-evaluation.loc[(evaluation['gender'] == 'male') & ~(eval0['age'] == 'twenties') & ~(eval0['age'] == 'teens')].to_csv(args.data + '/sw/eval_age_gender_o30s_male.csv', index=False, sep='\t')
+evaluation.loc[(evaluation['gender'] == 'female') & ~(eval0['age'] == 'twenties') & ~(eval0['age'] == 'teens')].to_csv(args.data + '/sw/eval_age_gender_o30s_female.tsv', index=False, sep='\t')
+evaluation.loc[(evaluation['gender'] == 'male') & ~(eval0['age'] == 'twenties') & ~(eval0['age'] == 'teens')].to_csv(args.data + '/sw/eval_age_gender_o30s_male.tsv', index=False, sep='\t')
