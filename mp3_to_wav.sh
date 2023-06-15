68fb712ca854d9a2e481fdf4dc5d43afee2a6a59
Ubuntu <ubuntu@ip-172-31-80-69.ec2.internal>
2023-06-15 09:52:14 +0000

    changed to the mp3 to wav conversion file for memory efficiency

--- mp3_to_wav.sh
+++ mp3_to_wav.sh
@@ -1,6 +1,11 @@
-for f in *.mp3; do ffmpeg -i "$f" -acodec pcm_s16le -ac 1 -ar 48000 "${f%%.*}-int.wav"; done 
-for f in *.wav; do ffmpeg -i "$f" -acodec pcm_s16le -ac 1 -ar 16000 "${f%%-*}.wav"; done
-rm *-rm *-int.wavint.wav
-mkdir clips_wav 
-find . -name '*.wav' | xargs mv --target-directory=clips_wav
+for f in *.mp3; 
+do 
+	ffmpeg -i "$f" -acodec pcm_s16le -ac 1 -ar 48000 "${f%%.*}-int.wav"; 
+	rm "$f"
+done 
+for f in *.wav; 
+do 
+	ffmpeg -i "$f" -acodec pcm_s16le -ac 1 -ar 16000 "${f%%-*}.wav"; 
+	rm "$f"
+done
 
